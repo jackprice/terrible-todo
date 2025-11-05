150f8af8a8995ae07fec8a3172552fffe475747f
Jack Price <jack.price@roadtohealthgroup.com>
2025-11-05 09:10:23 +0000

    Update description

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # TODO
 
-This is an example, deliberately buggy TODO app! 
+This is an example TODO app!
+
+## Running
+
+1. Clone the repository - `git clone https://github.com/jackprice/terrible-todo.git && cd terrible-todo`
+2. Install dependencies - `npm install`
+3. Start the app - `yarn start`
--- src/App.tsx
+++ src/App.tsx
@@ -40,12 +40,6 @@ function App() {
     dispatch(deleteTodo({ id }));
   };
 
-  const handleKeyPress = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      handleAddTodo();
-    }
-  };
-
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-8 px-4">
       <div className="w-full">
@@ -59,7 +53,6 @@ function App() {
               type="text"
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
-              onKeyPress={handleKeyPress}
               placeholder="Add a new todo..."
               className="flex-1 px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-gray-200"
             />
